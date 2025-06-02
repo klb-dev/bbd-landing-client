f0ae95a922f8158e68a15f86183deb30f212527f
Karen Byrd <klbdev88@gmail.com>
2025-06-02 14:44:27 -0500

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
 import { AuthProvider } from './context/AuthProvider.tsx';
+import { inject } from '@vercel/analytics';
 import './index.css';
 import App from './App.tsx';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
